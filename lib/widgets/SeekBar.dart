3b887eaa7da1372744f7d9eaf8f71dc0cfce09b9
耿其权 <542400966@qq.com>
2019-01-25 13:53:29 +0800

    seekBar

--- lib/widgets/SeekBar.dart
+++ lib/widgets/SeekBar.dart
@@ -1,170 +1,186 @@
-
 import 'package:flutter/material.dart';
-void main() => runApp(new SeekBar());
-/// A vertical SeekBar build on two [SizedBox] with versatile appearance.
+
+/// SeekBar build on two [SizedBox] .
 class SeekBar extends StatefulWidget {
   /// The width of the SeekBar. @required
   final double width;
+
   /// The height of the SeekBar. @required
   final double height;
+
   /// The minimum value of the SeekBar.
   ///
   /// Defaults to 0.0 .
   final double min;
+
   /// The maximum value of the SeekBar.
   ///
   /// Defaults to 10.0 .
   final double max;
+
   /// The step by which the SeekBar's value is incremented.
   ///
   /// Defaults to 1.0 .
-  final double step;
+//  final double step;
+
   /// The initial value of the SeekBar.
   ///
-  /// Defaults to [max] - [step].
+  /// Defaults to [min] - [step].
   final double value;
+
+  /// The initial value of the seek radius.
+  ///
+  /// Defaults to Math.min(width ,height) ].
+  final double radius;
+
   /// The sensitivity of the SeekBar's edges in pixels.
   ///
   /// A value of 5.0 means that when the SeekBar is less than 5 pixels away
   /// from the top or bottom, it will snap to it. Defaults to 0.0 .
-  final double accuracy;
+//  final double accuracy;
+
   /// The child [Widget] of the fixed [SizedBox].
   ///
-  /// Use this to change the appearance of the SeekBar. Defaults to a
-  /// [DecoratedBox] with a grey [BoxDecoration].
-  final Widget fixedBox;
+  /// Use this to change the appearance of the SeekBar. Defaults to h a grey [Container].
+  final Widget bar;
+
   /// The child [Widget] of the moving [SizedBox].
   ///
   /// Use this to change the appearance of the SeekBar. Defaults to a
-  /// [DecoratedBox] with a red [BoxDecoration].
-  final Widget movingBox;
+  /// [SizedBox] with a red [ClipOval].
+  final Widget seek;
+
   /// The function called when the value of the SeekBar changes.
   /// Passes the new value as a parameter.
   final void Function(double) onValueChanged;
 
-  const BoxedVerticalSeekBar({
+  const SeekBar({
     Key key,
     @required this.height,
     @required this.width,
     this.onValueChanged,
-    this.max,
-    this.min,
-    this.step,
-    this.accuracy,
-    this.value,
-    this.fixedBox,
-    this.movingBox,
+    this.max = 100.0,
+    this.min = 0.0,
+    this.radius,
+//    this.step,
+//    this.accuracy,
+    this.value = 0,
+    this.bar,
+    this.seek,
   }) : super(key: key);
-  
-  _BoxedVerticalSeekBarState createState() => _BoxedVerticalSeekBarState();
+
+  _SeekBarState createState() => _SeekBarState();
 }
 
-class _BoxedVerticalSeekBarState extends State<BoxedVerticalSeekBar>{
-  double _value;
-  double _min;
-  double _max;
-  double _step;
-  double _currentHeight;
-  double _accuracy;
-  Widget _movingDecoratedBox;
-  Widget _fixedDecoratedBox;
+class _SeekBarState extends State<SeekBar> {
+  double _length;
+  double _current = 0.0;
+  double radius;
+  Widget seek;
+  Widget bar;
+  bool isHorizontal = true;
 
   initState() {
     super.initState();
-    // Initialise all values.
-    _min = widget.min ?? 0.0;
-    _max = widget.max ?? 10.0;
-    _step = widget.step ?? 1.0;
-    _value = widget.value ?? 5.0;
-    _accuracy = widget.accuracy ?? 0.0;
-    _currentHeight = _convertValueToHeight();
-    _movingDecoratedBox = widget.movingBox ?? DecoratedBox(
-        decoration: BoxDecoration(color: Colors.red)
-    );
-    _fixedDecoratedBox = widget.fixedBox ?? DecoratedBox(
-      decoration: BoxDecoration(color: Colors.grey),
-    );
+    isHorizontal = widget.width > widget.height;
+
+    radius = widget.radius ?? (isHorizontal ? widget.height : widget.width);
+
+    _length = isHorizontal ? widget.width : widget.height;
+    _current = _convertLength();
   }
 
-  // Converts the current value to its respective height.
-  double _convertValueToHeight() {
-    return (_value - _min) * widget.height / (_max - _min);
+  double _convertLength() {
+    return (widget.value - widget.min) * _length / (widget.max - widget.min) -
+        radius;
   }
 
-  // Converts SizedBox height to its respective value.
-  double _convertHeightToValue(double height) {
-    var tempValue = height * (_max - _min) / widget.height + _min;
-    if (tempValue != _max && tempValue != _min) {
-      tempValue = tempValue - (tempValue % _step) + (_min % _step);
-    }
-    return tempValue;
-  }
-
-  // Updates height and value when user taps on the SeekBar.
-  void _onTapUp(TapUpDetails tapDetails) {
-    RenderBox renderBox = context.findRenderObject();
-    setState(() {
-      _currentHeight =
-          widget.height - renderBox.globalToLocal(tapDetails.globalPosition).dy;
-      _setValue(_convertHeightToValue(_currentHeight));
-    });
-  }
-
-  // Updates height and value when user drags the SeekBar.
-  void _onVerticalDragUpdate(DragUpdateDetails dragDetails) {
-    if (dragDetails.primaryDelta >= 1.0  ||  dragDetails.primaryDelta <= -1.0) {
-      var newHeight = _currentHeight - dragDetails.primaryDelta;
-      if (newHeight > (widget.height - _accuracy)) {
-        newHeight = widget.height;
-      } else if (newHeight < _accuracy) {
-        newHeight = 0.0;
-      }
-
-      if (newHeight != _currentHeight) {
-        setState(() {
-          _currentHeight = newHeight;
-          _setValue(_convertHeightToValue(_currentHeight));
-        });
-      }
-    }
-  }
-
-  void _setValue(double newValue) {
-    _value = newValue;
-    widget.onValueChanged(_value);
-  }
-
-  Widget _buildFixedBox() {
-    return SizedBox(
-      width: widget.width,
-      height: widget.height,
-      child: _fixedDecoratedBox,
-    );
-  }
-
-  Widget _buildMovingBox() {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: SizedBox(
-        width: widget.width,
-        height: _currentHeight,
-        child: _movingDecoratedBox,
-      ),
-    );
+  double _getNewValue() {
+    return (_current + radius) * (widget.max - widget.min) / _length +
+        widget.min;
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTapUp: _onTapUp,
-      onVerticalDragUpdate: _onVerticalDragUpdate,
+//      onTapUp: _onTapUp,
+      onHorizontalDragUpdate: (dragDetails) {
+        if (!isHorizontal) {
+          return;
+        }
+        setState(() {
+          var now = _current + dragDetails.delta.dx;
+          if (now > _length - radius) {
+            now = _length - radius;
+          }
+          if (now < -radius) {
+            now = -radius;
+          }
+          if (_current != now) {
+            _current = now;
+            var newVlaue = _getNewValue();
+            print("newVlaue:" + newVlaue.toString());
+            widget.onValueChanged ?? (newVlaue);
+          }
+        });
+      },
+      onVerticalDragUpdate: (dragDetails) {
+        if (isHorizontal) {
+          return;
+        }
+        setState(() {
+          var now = _current + dragDetails.delta.dy;
+          if (now > _length - radius) {
+            now = _length - radius;
+          }
+          if (now < -radius) {
+            now = -radius;
+          }
+          if (_current != now) {
+            _current = now;
+            var newVlaue = _getNewValue();
+            print("newVlaue:" + newVlaue.toString());
+            widget.onValueChanged ?? (newVlaue);
+          }
+        });
+      },
       child: Stack(
-        alignment: AlignmentDirectional.bottomCenter,
+        alignment: isHorizontal
+            ? AlignmentDirectional.centerStart
+            : AlignmentDirectional.topCenter,
         children: <Widget>[
-          _buildFixedBox(),
-          _buildMovingBox(),
+          _buildBar(),
+          new Transform.translate(
+            offset: Offset(
+                isHorizontal ? _current : 0, isHorizontal ? 0 : _current),
+            child: _buildSeek(),
+          ),
         ],
       ),
+    );
+  }
+
+  _buildSeek() {
+    return widget.seek ??
+        new SizedBox(
+          width: 2 * radius,
+          height: 2 * radius,
+          child: new ClipOval(
+            child: new Container(
+              color: Colors.red,
+            ),
+          ),
+        );
+  }
+
+  _buildBar() {
+    return new SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: widget.bar ??
+          new Container(
+            color: Colors.grey,
+          ),
     );
   }
 }
