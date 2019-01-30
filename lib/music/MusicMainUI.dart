9c0c240ce9872cc6ba5675cdc7c5409557a2d12c
耿其权 <542400966@qq.com>
2019-01-30 09:44:07 +0800

    music lrc

--- lib/music/MusicMainUI.dart
+++ lib/music/MusicMainUI.dart
@@ -3,35 +3,54 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'MusicDetailUI.dart';
 import 'package:beauty/main.dart';
+import 'dart:ui';
 
-class MusicMainUI extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-        home: new Scaffold(
-      appBar: new AppBar(
-        title: const Text("音乐"),
-      ),
-      drawer: new Drawer(
-        child: HomeBuilder.homeDrawer(context),
-      ),
-      body: MusicList(),
-    ));
-  }
-}
-
-class MusicList extends StatefulWidget {
+class MusicMainUI extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _MusicListState();
 }
 
-class _MusicListState extends State<MusicList> {
+class _MusicListState extends State<MusicMainUI> {
   Dio dio = new Dio();
   List list = new List();
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    return new MaterialApp(
+        home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text("音乐"),
+      ),
+//      appBar: new PreferredSize(
+//        child: new Container(
+//          height: 48,
+//          margin: EdgeInsets.only(
+//              top: MediaQueryData.fromWindow(window).padding.top),
+//          child: searchBar(),
+//        ),
+//        preferredSize: Size(double.infinity,
+//            48 + MediaQueryData.fromWindow(window).padding.top),
+//      ),
+      drawer: new Drawer(
+        child: HomeBuilder.homeDrawer(context),
+      ),
+      body: musicList(),
+    ));
+  }
+
+  Widget searchBar() {
+    return new TextField(
+//      textAlign: TextAlign.center,
+      decoration: new InputDecoration(
+        prefixIcon: new Icon(Icons.search),
+        hintText: "请输入歌名",
+//        prefixText: "请输入歌名:",
+        prefix: new Icon(Icons.search),
+      ),
+    );
+  }
+
+  Widget musicList() {
     return new RefreshIndicator(
         onRefresh: () => getHttp(),
         child: new ListView.builder(
